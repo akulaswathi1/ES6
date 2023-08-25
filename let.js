0e98049e1e60ed69d0d654fe79da06b22746edf9
AKULA SWATHI <143166122+akulaswathi1@users.noreply.github.com>
2023-08-26 05:06:44 +0530

    Create let.js
    - It defines block scope variable
    - It allows only initialization.
    - It will not allow declaring and assigning.
    - It will not allow hoisting and shadowing.

---
+++ let.js
@@ -0,0 +1,14 @@
+<script>
+    "use strict";
+    function f1(){
+       let x;               // declaring
+       x = 10;              // assigning
+       if(x==10)
+       {
+          var y = 20;        // initialization
+          document.write("X=" + x + "<br>" + "Y=" + y);
+       }
+       
+    }
+    f1();
+</script>
